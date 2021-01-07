13221e4a8db6db6cf7ef73da1758c2cf7a7dd2f9
Lethworgen <Lethworgen@hotmail.com>
2021-01-07 13:26:50 -0800

    Version 2. Fixed Positioning of Player car

--- BeginnerTutorial.py
+++ BeginnerTutorial.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from pygame.locals import *
-import random
+import random, time
 
 # Game Loop Skeleton
 # Changes in the game are not implemented until display.update() has been called
@@ -26,7 +26,7 @@ while True:
 # Initalizing a program
 pygame.init()
 
-# Asign FPS a value
+# Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
@@ -40,6 +40,9 @@ WHITE = (255, 255, 255)
 # Setup a 400x600 pixel display with caption
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
+SPEED = 5
+
+# Create a white screen
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
@@ -51,16 +54,13 @@ class Enemy(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("Enemy.png")
         self.surf = pygame.Surface((50, 80))
-        self.rect = self.surf.get_rect(center=(random.randint(40, 360), 0))
+        self.rect = self.surf.get_rect(center = (random.randint(40,SCREEN_WIDTH-40), 0))
 
     def move(self):
-        self.rect.move_ip(0, 10)
-        if self.rect.bottom > 600:
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > 600:
             self.rect.top = 0
-            self.rect.center = (random.randint(30, 370), 0)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+            self.rect.center = (random.randint(30,370), 0)
 
 
 # Defining the Player Class
@@ -69,9 +69,9 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("Player.png")
         self.surf = pygame.Surface((50, 100))
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center = (150, 550))
 
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
 #        if pressed_keys[K_UP]:
 #            self.rect.move_ip(0, -5)
@@ -84,24 +84,51 @@ class Player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
+# Setting up Sprites
 P1 = Player()
 E1 = Enemy()
 
+# Creating Sprites Groups
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+
+# Adding a new User event
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+# Game Loop
 while True:
+
+    #Cycles through all events occuring
     for event in pygame.event.get():
+        if event in pygame.event.get():
+            if event.type == INC_SPEED:
+                SPEED += 2
+
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-    P1.update()
-    E1.move()
-
     DISPLAYSURF.fill(WHITE)
-    P1.draw(DISPLAYSURF)
-    E1.draw(DISPLAYSURF)
+
+    # Moves and Re-draws all Sprites
+    for entity in all_sprites:
+        DISPLAYSURF.blit(entity.image, entity.rect)
+        entity.move
+
+    # Collision Detection
+    if pygame.sprite.spritecollideany(P1, enemies):
+        DISPLAYSURF.fill(RED)
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
 
     pygame.display.update()
     FramePerSec.tick(FPS)
